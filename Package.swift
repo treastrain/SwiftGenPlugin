a968ebcd1c0b803b31b52f476e773bd7fa92e64b
treastrain / Tanaka Ryoga <tr@tret.jp>
2023-09-06 03:43:47 +0900

    treastrain/SwiftGen Release support-existential-any

--- Package.swift
+++ Package.swift
@@ -29,8 +29,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "swiftgen",
-      url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.2/swiftgen-6.6.2.artifactbundle.zip",
-      checksum: "7586363e24edcf18c2da3ef90f379e9559c1453f48ef5e8fbc0b818fbbc3a045"
+      url: "https://github.com/treastrain/SwiftGen/releases/download/support-existential-any/swiftgen-support-existential-any.artifactbundle.zip",
+      checksum: "ca12666066ce6e6ec7681a9d3183e002d4c32fd623e2feacc949cf38a05dee66"
     )
   ]
 )
